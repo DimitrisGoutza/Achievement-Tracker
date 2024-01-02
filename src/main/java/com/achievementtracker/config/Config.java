213cca4baeadfe3c9779a5a483bb2dd593b033f8
DimitrisGoutza <opticsearch@hotmail.com>
2024-01-02 13:08:27 +0200

    Add partial support for external Steam-API -Instruct configuration where to look for Feign Clients -Add interface (contract) for Steam API

--- src/main/java/com/achievementtracker/config/Config.java
+++ src/main/java/com/achievementtracker/config/Config.java
@@ -1,9 +1,11 @@
 package com.achievementtracker.config;
 
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients(basePackages = {"com.achievementtracker.proxy"})
 public class Config {
 
 }
---
+++ src/main/java/com/achievementtracker/proxy/SteamApiProxy.java
@@ -0,0 +1,7 @@
+package com.achievementtracker.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient
+public interface SteamApiProxy {
+}
