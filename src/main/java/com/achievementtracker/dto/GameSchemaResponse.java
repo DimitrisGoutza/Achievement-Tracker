db9fdb7958318c4bcc02418020c8de95e34b320b
DimitrisGoutza <opticsearch@hotmail.com>
2024-01-04 16:14:41 +0200

    Update SteamGlobalAchievementStatsProxy interface: -Add method to fetch Game Schema by AppId (= GameId) -Add DTOs to bind JSON response from new method -Update properties file to accommodate new method mapping -Refactor proxy's bean name to be more accurate

---
+++ src/main/java/com/achievementtracker/dto/GameSchemaResponse.java
@@ -0,0 +1,140 @@
+package com.achievementtracker.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+public class GameSchemaResponse {
+    @JsonProperty("game")
+    private Game game;
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public String toString() {
+        return "GameSchemaResponse{" +
+                "game=" + game +
+                '}';
+    }
+
+    public static class Game {
+        @JsonProperty("gameName")
+        private String name;
+        @JsonProperty("availableGameStats")
+        private AvailableGameStats availableGameStats;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public AvailableGameStats getAvailableGameStats() {
+            return availableGameStats;
+        }
+
+        public void setAvailableGameStats(AvailableGameStats availableGameStats) {
+            this.availableGameStats = availableGameStats;
+        }
+
+        @Override
+        public String toString() {
+            return "Game{" +
+                    "name='" + name + '\'' +
+                    ", availableGameStats=" + availableGameStats +
+                    '}';
+        }
+
+        public static class AvailableGameStats {
+            @JsonProperty("achievements")
+            private Map<String, AchievementDetail> achievements;
+
+            public Map<String, AchievementDetail> getAchievements() {
+                return achievements;
+            }
+
+            public void setAchievements(Map<String, AchievementDetail> achievements) {
+                this.achievements = achievements;
+            }
+
+            @Override
+            public String toString() {
+                return "AvailableGameStats{" +
+                        "achievements=" + achievements +
+                        '}';
+            }
+
+            public static class AchievementDetail {
+                @JsonProperty("displayName")
+                private String name;
+                @JsonProperty("hidden")
+                private int hidden;
+                @JsonProperty("description")
+                private String description;
+                @JsonProperty("icon")
+                private String iconUrl;
+                @JsonProperty("icongray")
+                private String iconGrayUrl;
+
+                public String getName() {
+                    return name;
+                }
+
+                public void setName(String name) {
+                    this.name = name;
+                }
+
+                public int getHidden() {
+                    return hidden;
+                }
+
+                public void setHidden(int hidden) {
+                    this.hidden = hidden;
+                }
+
+                public String getDescription() {
+                    return description;
+                }
+
+                public void setDescription(String description) {
+                    this.description = description;
+                }
+
+                public String getIconUrl() {
+                    return iconUrl;
+                }
+
+                public void setIconUrl(String iconUrl) {
+                    this.iconUrl = iconUrl;
+                }
+
+                public String getIconGrayUrl() {
+                    return iconGrayUrl;
+                }
+
+                public void setIconGrayUrl(String iconGrayUrl) {
+                    this.iconGrayUrl = iconGrayUrl;
+                }
+
+                @Override
+                public String toString() {
+                    return "AchievementDetail{" +
+                            "name='" + name + '\'' +
+                            ", hidden=" + hidden +
+                            ", description='" + description + '\'' +
+                            ", iconUrl='" + iconUrl + '\'' +
+                            ", iconGrayUrl='" + iconGrayUrl + '\'' +
+                            '}';
+                }
+            }
+        }
+    }
+}
--- src/main/java/com/achievementtracker/proxy/SteamGlobalStatsProxy.java
+++ src/main/java/com/achievementtracker/proxy/SteamGlobalStatsProxy.java
@@ -2,15 +2,19 @@ package com.achievementtracker.proxy;
 
 import com.achievementtracker.dto.AchievementResponse;
 import com.achievementtracker.dto.AppListResponse;
+import com.achievementtracker.dto.GameSchemaResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "steam-api-global-stats", url = "${steam.api.url}")
+@FeignClient(name = "steam-api-global-data", url = "${steam.api.url}")
 public interface SteamGlobalStatsProxy {
     @GetMapping("${global.achievement.stats.endpoint}" + "/?gameid={gameId}&format=json")
     AchievementResponse fetchAchievementStatsByGameId(@RequestParam("gameid") int gameId);
 
     @GetMapping("${app.list.endpoint}" + "/")
     AppListResponse fetchAllApps();
+
+    @GetMapping("${game.schema.endpoint}" + "/?key=${steam.api.key}&appid={appId}")
+    GameSchemaResponse fetchSchemaByAppId(@RequestParam("appid") int appId);   // appid is the same as gameid
 }
