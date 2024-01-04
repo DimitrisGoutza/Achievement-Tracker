ae575f1152748cc9e5f92ff82407478223db5518
DimitrisGoutza <opticsearch@hotmail.com>
2024-01-04 15:09:06 +0200

    Reorganize DTO class structure for better file organization -Combine AchievementResponse, AchievementPercentages and Achievement classes into a single file -Combine AppListResponse, AppList and App classes into a single file -Make nested classes static to achieve independence

--- src/main/java/com/achievementtracker/dto/AchievementResponse.java
+++ src/main/java/com/achievementtracker/dto/AchievementResponse.java
@@ -2,6 +2,8 @@ package com.achievementtracker.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class AchievementResponse {
     @JsonProperty("achievementpercentages")
     private AchievementPercentages achievementPercentages;
@@ -19,5 +21,56 @@ public class AchievementResponse {
         return "AchievementResponse{" +
                 "achievementPercentages=" + achievementPercentages +
                 '}';
+    }
+
+    public static class AchievementPercentages {
+        @JsonProperty("achievements")
+        List<Achievement> achievements;
+
+        public List<Achievement> getAchievements() {
+            return achievements;
+        }
+
+        public void setAchievements(List<Achievement> achievements) {
+            this.achievements = achievements;
+        }
+
+        @Override
+        public String toString() {
+            return "AchievementPercentages{" +
+                    "achievements=" + achievements +
+                    '}';
+        }
+
+        public static class Achievement {
+            @JsonProperty("name")
+            private String name;
+            @JsonProperty("percent")
+            private double percentage;
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public double getPercentage() {
+                return percentage;
+            }
+
+            public void setPercentage(double percentage) {
+                this.percentage = percentage;
+            }
+
+            @Override
+            public String toString() {
+                return "Achievement{" +
+                        "name='" + name + '\'' +
+                        ", percentage=" + percentage +
+                        '}';
+            }
+        }
     }
 }
--- src/main/java/com/achievementtracker/dto/AppListResponse.java
+++ src/main/java/com/achievementtracker/dto/AppListResponse.java
@@ -2,6 +2,8 @@ package com.achievementtracker.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public class AppListResponse {
     @JsonProperty("applist")
     private AppList appList;
@@ -19,5 +21,56 @@ public class AppListResponse {
         return "AppListResponse{" +
                 "appList=" + appList +
                 '}';
+    }
+
+    public static class AppList {
+        @JsonProperty("apps")
+        private List<App> apps;
+
+        public List<App> getApps() {
+            return apps;
+        }
+
+        public void setApps(List<App> apps) {
+            this.apps = apps;
+        }
+
+        @Override
+        public String toString() {
+            return "AppList{" +
+                    "apps=" + apps +
+                    '}';
+        }
+
+        public static class App {
+            @JsonProperty("appid")
+            private int id;
+            @JsonProperty("name")
+            private String name;
+
+            public int getId() {
+                return id;
+            }
+
+            public void setId(int id) {
+                this.id = id;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            @Override
+            public String toString() {
+                return "App{" +
+                        "id=" + id +
+                        ", name='" + name + '\'' +
+                        '}';
+            }
+        }
     }
 }
