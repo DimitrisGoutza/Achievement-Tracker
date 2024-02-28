9fe97c31c5de5fcb3e144bd50313e92ff717dd3d
DimitrisGoutza <opticsearch@hotmail.com>
2024-02-28 06:24:14 +0200

    Add new GameDAO method - This method will be used to fetch records (with eagerly loaded associated collections) and modify them internally (e.g. Updating the database). - First fetches the IDs of all games and then the actual records. This prevents an optimization issue Hibernate has when using JOIN FETCH and setMaxResults(). Otherwise all records are loaded into memory and the pagination applies after. Instead we paginate the ID of the records first and then we specify a query that includes these IDs.

--- src/main/java/com/achievementtracker/dao/GameDAO.java
+++ src/main/java/com/achievementtracker/dao/GameDAO.java
@@ -1,7 +1,11 @@
 package com.achievementtracker.dao;
 
 import com.achievementtracker.entity.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface GameDAO extends GenericDAO<Game, Long> {
     Game retrieveLatestEntry();
+
+    Page<Game> findAllWithCollections(Pageable pageable);
 }
--- src/main/java/com/achievementtracker/dao/GameDAOImpl.java
+++ src/main/java/com/achievementtracker/dao/GameDAOImpl.java
@@ -3,7 +3,12 @@ package com.achievementtracker.dao;
 import com.achievementtracker.entity.Game;
 import jakarta.persistence.NoResultException;
 import jakarta.persistence.TypedQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class GameDAOImpl extends GenericDAOImpl<Game, Long> implements GameDAO {
@@ -20,5 +25,25 @@ public class GameDAOImpl extends GenericDAOImpl<Game, Long> implements GameDAO {
         } catch (NoResultException e) {
             return null;
         }
+    }
+
+    @Override
+    public Page<Game> findAllWithCollections(Pageable pageable) {
+        TypedQuery<Long> queryForIDs = em.createQuery("SELECT storeId FROM Game", Long.class);
+
+        int offset = pageable.getPageNumber() * pageable.getPageSize();
+        queryForIDs.setFirstResult(offset);
+        queryForIDs.setMaxResults(pageable.getPageSize());
+        List<Long> gameIds = queryForIDs.getResultList();
+
+        TypedQuery<Game> query = em.createQuery("FROM Game g LEFT JOIN FETCH " +
+                "g.achievements LEFT JOIN FETCH " +
+                "g.categorizedGames cg LEFT JOIN FETCH " +
+                "cg.category " +
+                "WHERE g.storeId IN :gameIds", Game.class);
+        query.setParameter("gameIds", gameIds);
+        List<Game> games = query.getResultList();
+
+        return new PageImpl<>(games, pageable, getCount());
     }
 }
