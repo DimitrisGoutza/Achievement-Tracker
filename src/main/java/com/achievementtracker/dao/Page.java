9521708af1cabfba37add49d1a29cca206fd2776
DimitrisGoutza <opticsearch@hotmail.com>
2024-03-07 14:37:57 +0200

    Create custom abstract class for pagination (Page) - Is an abstract class which encapsulates common attributes and methods shared among various pagination implementations (e.g. Offset, Seek). - Specifically designed for use with Criteria Query API to aid the creation of dynamic queries. - Defines one abstract method (createQuery) that has to be implemented by specific Page subclasses (e.g. Offset, Seek). This method will dynamically incorporate pagination and sorting attributes/direction into an existing criteria query.

---
+++ src/main/java/com/achievementtracker/dao/Page.java
@@ -0,0 +1,110 @@
+package com.achievementtracker.dao;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Path;
+import jakarta.persistence.metamodel.Bindable;
+import jakarta.persistence.metamodel.SingularAttribute;
+
+import java.util.Arrays;
+
+public abstract class Page {
+    public static enum SortDirection {
+        ASC,
+        DESC
+    }
+
+    // value of -1 means "no limit, show all records"
+    protected int size = -1;
+    protected long totalRecords;
+    protected SingularAttribute sortAttribute;
+    protected SortDirection sortDirection;
+    protected SingularAttribute[] allowedAttributes;
+
+    protected Page(int size, long totalRecords,
+                   SingularAttribute defaultAttribute,SortDirection defaultDirection,
+                   SingularAttribute... allowedAttributes) {
+        this.size = size;
+        this.totalRecords = totalRecords;
+        this.sortDirection = defaultDirection;
+        this.allowedAttributes = allowedAttributes;
+        setSortAttribute(defaultAttribute);
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public long getTotalRecords() {
+        return totalRecords;
+    }
+
+    public void setTotalRecords(long totalRecords) {
+        this.totalRecords = totalRecords;
+    }
+
+    public SingularAttribute getSortAttribute() {
+        return sortAttribute;
+    }
+
+    public SortDirection getSortDirection() {
+        return sortDirection;
+    }
+
+    public SingularAttribute[] getAllowedAttributes() {
+        return allowedAttributes;
+    }
+
+    public void setSortDirection(SortDirection sortDirection) {
+        this.sortDirection = sortDirection;
+    }
+
+    public boolean isSortedAscending() {
+        return sortDirection.ASC.equals(getSortDirection());
+    }
+
+    public void setAllowedAttributes(SingularAttribute[] allowedAttributes) {
+        this.allowedAttributes = allowedAttributes;
+    }
+
+    public void setSortAttribute(SingularAttribute attribute) {
+        if (attribute == null)
+            return;
+        if (!Arrays.asList(allowedAttributes).contains(attribute))
+            throw new IllegalArgumentException(
+                    "Sorting by attribute not allowed " + attribute.getName()
+            );
+        this.sortAttribute = attribute;
+    }
+
+    public boolean isMoreThanOneAvailable() {
+        return getTotalRecords() != 0 && getTotalRecords() > getSize();
+    }
+
+    public boolean isAttributeDeclaredIn(SingularAttribute attribute, Bindable bindable) {
+        return attribute != null && attribute.getDeclaringType().equals(bindable);
+    }
+
+    public boolean isApplicableFor(Bindable bindable) {
+        return isAttributeDeclaredIn(getSortAttribute(), bindable);
+    }
+
+    public void throwIfNotApplicableFor(Path attributePath) {
+        if (!isApplicableFor(attributePath.getModel())) {
+            throw new IllegalArgumentException(
+                    "Paging settings/sort attribute are not declared by model of query path: " + attributePath
+            );
+        }
+    }
+
+    abstract public <T>TypedQuery<T> createQuery(
+            EntityManager em,
+            CriteriaQuery<T> criteriaQuery,
+            Path attributePath
+    );
+}
