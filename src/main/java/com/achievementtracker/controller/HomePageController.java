33f88fec14d60908d84c11bef958ced9a6a3f581
DimitrisGoutza <opticsearch@hotmail.com>
2024-03-17 03:18:19 +0200

    Add pagination features for home page (offset type) - Whenever a user wants to change the page size, we don't pass the page number he is currently on in the URL params. We go back to the first page of entries. However when he decides to change page, we also pass the selected page size in the params.

--- src/main/java/com/achievementtracker/controller/HomePageController.java
+++ src/main/java/com/achievementtracker/controller/HomePageController.java
@@ -1,28 +1,45 @@
 package com.achievementtracker.controller;
 
 import com.achievementtracker.dao.GameDAO;
+import com.achievementtracker.dao.OffsetPage;
+import com.achievementtracker.dao.Page;
 import com.achievementtracker.entity.Game;
+import com.achievementtracker.entity.Game_;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class HomePageController {
     private final GameDAO gameDAO;
+    private final OffsetPage page;
 
     @Autowired
     public HomePageController(GameDAO gameDAO) {
         this.gameDAO = gameDAO;
+
+        this.page = new OffsetPage(50, gameDAO.getCount(),
+                Game_.challengeRating, Page.SortDirection.DESC,
+                Game_.storeId, Game_.steamAppId, Game_.title, Game_.releaseDate,
+                Game_.challengeRating, Game_.averageCompletion, Game_.difficultySpread);
     }
 
     @GetMapping("/home")
-    public String getHomePage(Model model) {
-        List<Game> games = gameDAO.findAll();
+    public String getHomePage(@RequestParam(name = "page") Optional<Integer> pageOptional,
+                              @RequestParam(name = "size") Optional<Integer> sizeOptional,
+                              Model model) {
+        page.setCurrent(pageOptional.orElse(1));
+        page.setSize(sizeOptional.orElse(50));
+
+        List<Game> games = gameDAO.findAll(page);
 
         model.addAttribute("games", games);
+        model.addAttribute("page", page);
 
         return "home";
     }
---
+++ src/main/resources/static/pagination.js
@@ -0,0 +1,63 @@
+function changePageSize() {
+    const determineEndpointURL = () => {
+        const selectedPageSize = document.getElementById("page-entries-select").value;
+
+        const fetchURL = new URL(window.location.origin + window.location.pathname + window.location.search);
+
+        const newParams = new URLSearchParams();
+        newParams.set("size", selectedPageSize);
+
+        fetchURL.search = newParams.toString();
+        return fetchURL;
+    };
+
+    fetch(determineEndpointURL())
+        .then(response => response.text())
+        .then(html => {
+            const fragment = document.createRange().createContextualFragment(html);
+            const updatedTableBody = fragment.getElementById("table-content");
+            const updatedPageFooter = fragment.getElementById("pagination-footer");
+
+            // update the relevant elements only
+            document.getElementById("table-content").innerHTML = updatedTableBody.innerHTML;
+            document.getElementById("pagination-footer").innerHTML = updatedPageFooter.innerHTML;
+        })
+        .catch(error => console.error("Error: "+error));
+}
+
+function changePageNumber(requestPageNumber) {
+    const determineEndpointURL = () => {
+        const selectedPageSize = document.getElementById("page-entries-select").value;
+
+        const fetchURL = new URL(window.location.origin + window.location.pathname + window.location.search);
+
+        const newParams = new URLSearchParams();
+        newParams.set("size", selectedPageSize);
+        newParams.set("page", requestPageNumber);
+
+        fetchURL.search = newParams.toString();
+        return fetchURL;
+    };
+
+    fetch(determineEndpointURL())
+        .then(response => response.text())
+        .then(html => {
+            const fragment = document.createRange().createContextualFragment(html);
+            const updatedTableBody = fragment.getElementById("table-content");
+            const updatedPageFooter = fragment.getElementById("pagination-footer");
+
+            // update the relevant elements only
+            document.getElementById("table-content").innerHTML = updatedTableBody.innerHTML;
+            document.getElementById("pagination-footer").innerHTML = updatedPageFooter.innerHTML;
+
+            scrollToTop();
+        })
+        .catch(error => console.error("Error: "+error));
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
