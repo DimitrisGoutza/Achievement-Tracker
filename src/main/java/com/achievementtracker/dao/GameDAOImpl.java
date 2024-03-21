9b6b5adcbe520a1beaf26b79c7b399d0a3a0eaa0
DimitrisGoutza <opticsearch@hotmail.com>
2024-03-21 04:35:03 +0200

    Refactor GameDAOImpl to be more readable - In this version, we use Criteria Query only when it's needed. Criteria Query is needed when we wish to eventually augment the query with Offset page features (pagination, sorting). Following that rule, every query that doesn't need those features uses JPQL. Consequently, the queries become more readable.

--- src/main/java/com/achievementtracker/dao/GameDAOImpl.java
+++ src/main/java/com/achievementtracker/dao/GameDAOImpl.java
@@ -33,42 +33,38 @@ public class GameDAOImpl extends GenericDAOImpl<Game, Long> implements GameDAO {
         CriteriaBuilder cb = em.getCriteriaBuilder();
 
         // First we retrieve the Game IDs paginated
-        CriteriaQuery<Long> criteriaQueryForIds = cb.createQuery(Long.class);
+        CriteriaQuery<Long> cqForIds = cb.createQuery(Long.class);
+        // FROM Game
+        Root<Game> idRoot = cqForIds.from(Game.class);
+        // SELECT g.storeId
+        cqForIds.select(idRoot.get(Game_.storeId));
 
-        Root<Game> idRoot = criteriaQueryForIds.from(Game.class);
-        criteriaQueryForIds.select(idRoot.get(Game_.storeId));
-
-        TypedQuery<Long> queryForIds = page.createQuery(em, criteriaQueryForIds, idRoot);
+        TypedQuery<Long> queryForIds = page.createQuery(em, cqForIds, idRoot);
         List<Long> gameIds = queryForIds.getResultList();
 
         // Then we the retrieve Games (along with eagerly loaded collections) that correspond to those IDs
-        CriteriaQuery<Game> criteriaQueryForGames = cb.createQuery(Game.class);
-        // FROM Game
-        Root<Game> gameRoot = criteriaQueryForGames.from(Game.class);
-        // JOIN FETCH associated collections
-        gameRoot.fetch(Game_.achievements, JoinType.LEFT);
-        Fetch<Game, CategorizedGame> categorizedGameFetch = gameRoot.fetch(Game_.categorizedGames, JoinType.LEFT);
-        categorizedGameFetch.fetch(CategorizedGame_.category, JoinType.LEFT);
-        // WHERE Game.storeId IN gameIds
-        criteriaQueryForGames.where(gameRoot.get(Game_.storeId).in(gameIds));
-        // PROJECTION
-        criteriaQueryForGames.select(gameRoot).distinct(true);
-
-        TypedQuery<Game> query = em.createQuery(criteriaQueryForGames);
+        TypedQuery<Game> query = em.createQuery("SELECT g FROM Game g " +
+                " LEFT JOIN FETCH g.achievements" +
+                " LEFT JOIN FETCH g.categorizedGames cg" +
+                " LEFT JOIN FETCH cg.category" +
+                " WHERE g.storeId IN :gameIds", Game.class);
+        query.setParameter("gameIds", gameIds);
         return query.getResultList();
     }
 
     @Override
     public List<Game> findAll(Page page) {
+        // Count query
+        TypedQuery<Long> queryForCount = em.createQuery("SELECT COUNT(g) FROM Game g", Long.class);
+        Long totalRecordCount = queryForCount.getSingleResult();
+        page.setTotalRecords(totalRecordCount);
+
+        // Main query
         CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Game> cq = cb.createQuery(Game.class);
+        Root<Game> gameRoot = cq.from(Game.class);
 
-        CriteriaQuery<Game> criteriaQuery = cb.createQuery(Game.class);
-        // FROM Game
-        Root<Game> gameRoot = criteriaQuery.from(Game.class);
-        // PROJECTION
-        criteriaQuery.select(gameRoot);
-
-        TypedQuery<Game> query = page.createQuery(em, criteriaQuery, gameRoot);
+        TypedQuery<Game> query = page.createQuery(em, cq, gameRoot);
         return query.getResultList();
     }
 }
