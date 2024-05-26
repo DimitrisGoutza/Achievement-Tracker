a07fd196ef3940dd992149e41b5263666d00028e
DimitrisGoutza <opticsearch@hotmail.com>
2024-05-26 11:56:51 +0300

    Add carousel func for viewing achievements - User has 3 ways of viewing them in the table. He can either click for fixed amount of scroll, hold for continuous scroll or drag the scrollbar.

--- src/main/resources/static/js/gameDetails.js
+++ src/main/resources/static/js/gameDetails.js
@@ -1,12 +1,45 @@
 /* ---------------------- Useful Element References ---------------------- */
 const achievementTierTable = document.getElementById("achievement-tier-table");
+const achievementContainers = achievementTierTable.querySelectorAll("div.achievements-container");
+const carouselButtons = achievementTierTable.querySelectorAll("button.carousel-scroll-button");
 /* ---------------------- Important Declarations ---------------------- */
-
+//region Carousel
+const ScrollBehavior = { SMOOTH: "smooth", INSTANT: "instant" };
+let continuousScrollInterval;
+let mousedownDurationInterval;
+//endregion
 /* ---------------------- DOMContentLoaded Actions ---------------------- */
-showMessageForEmptyTiers();
 replacePlaceholderImages();
+showMessageForEmptyTiers();
+//region Carousel
+makeCarouselScrollButtonsVisible();
+disableOrEnableCarouselButtons();
+//endregion
 /* ---------------------- Event Listeners ---------------------- */
+//region Carousel
+carouselButtons.forEach(button => {
+    button.addEventListener("mousedown", (event) => {
+        const mousedownTimestamp = performance.now();
 
+        scrollAchievementsContainer(button, 300, ScrollBehavior.SMOOTH);
+        mousedownDurationInterval = setInterval(function periodicallyCheckDuration() {
+            if (performance.now() - mousedownTimestamp >= 400) {
+                clearInterval(mousedownDurationInterval); // we no longer need to check
+                startContinuousScrolling(event);
+            }
+        }, 50);
+    });
+    button.addEventListener("mouseup", () => {
+        clearInterval(mousedownDurationInterval); // user no longer holds down click
+        stopContinuousScrolling();
+    });
+    button.addEventListener("mouseleave", () => {
+        clearInterval(mousedownDurationInterval);
+        stopContinuousScrolling();
+    });
+});
+achievementContainers.forEach(container => container.addEventListener("scroll", () => disableOrEnableCarouselButtons()));
+//endregion
 /* ---------------------- Function Declarations ---------------------- */
 function replacePlaceholderImages() {
     const imageElements = document.querySelectorAll("img.achievement-icon");
@@ -22,7 +55,50 @@ function showMessageForEmptyTiers() {
     const achievementContainers = achievementTierTable.querySelectorAll(".achievements-container");
     achievementContainers.forEach(container => {
         const achievements = container.querySelectorAll("img.achievement-icon");
-        if (achievements.length === 0)
-            container.querySelector("span.empty-tier-message").style.display = "";
+        if (achievements.length === 0) {
+            container.querySelector("span.empty-tier-message").style.display = "block";
+            container.parentElement.style.justifyContent = "center";
+        }
     })
 }
+//region Carousel
+function scrollAchievementsContainer(button, scrollAmount, scrollBehavior) {
+    const direction = button.dataset.direction;
+    const container = button.parentNode.querySelector("div.achievements-container");
+
+    if (direction === "left")
+        container.scrollBy({left: -scrollAmount, behavior: scrollBehavior});
+    else
+        container.scrollBy({left: scrollAmount, behavior: scrollBehavior});
+}
+function startContinuousScrolling(event) {
+    const button = event.target;
+    // basically repeat instant scrolling very often
+    continuousScrollInterval = setInterval(() => {
+        scrollAchievementsContainer(button, 10, ScrollBehavior.INSTANT);
+    }, 5);
+}
+function stopContinuousScrolling() {
+    clearInterval(continuousScrollInterval);
+}
+function makeCarouselScrollButtonsVisible() {
+    carouselButtons.forEach(button => {
+        const container = button.parentNode.querySelector("div.achievements-container");
+        const isOverflowing = container.scrollWidth > container.clientWidth;
+        if (isOverflowing) {
+            // Left Button
+            container.previousElementSibling.style.display = "block";
+            // Right Button
+            container.nextElementSibling.style.display = "block";
+        }
+    })
+}
+function disableOrEnableCarouselButtons() {
+    achievementContainers.forEach(container => {
+        const scrollLeftButton = container.previousElementSibling;
+        const scrollRightButton = container.nextElementSibling;
+        scrollLeftButton.disabled = container.scrollLeft === 0;
+        scrollRightButton.disabled = container.scrollLeft + container.clientWidth >= container.scrollWidth;
+    });
+}
+//endregion
